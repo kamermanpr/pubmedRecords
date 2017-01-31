27d24cc4045332539ea08555d39ca0e9147fe54b
Peter Kamerman <peter.kamerman@gmail.com>
2017-01-31 16:45:46 +0200

    Added more undefined variables to global environment.

--- R/on_load.R
+++ R/on_load.R
@@ -1,5 +1,5 @@
 # This user-invisible function defines global variables when the package loads,
-# thus avoiding \emph{'no visible binding for global variable'} notes when
+# thus avoiding 'no visible binding for global variable' notes when
 # running R CMD check.
 
 .onLoad <- function(libname = find.package('pubmedRecords'),
@@ -10,7 +10,10 @@
             # Based on initial R CMD check
             c('.', 'affiliation', 'article_node', 'author_node', 'authors',
               'count', 'country_iso3', 'country_name', 'initials', 'n',
-              'order2', 'state_name', 'surname', 'value'))
+              'order2', 'state_name', 'surname', 'value', 'pmid', 'doi',
+              'pmid2', 'score', 'cited_by_tweeters_count',
+              'cited_by_fbwalls_count', 'cited_by_posts_count',
+              'mendeley', 'crossref_citations', 'publication_status'))
 
     invisible()
 }
